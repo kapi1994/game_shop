8f35033e5d364685ab9988479f0a45297df49ed3
Nemanja Jovicic <nemanjajovicic13@gmail.com>
2024-10-08 18:19:01 +0200

    get one link

---
+++ models/links/edit.php
@@ -0,0 +1,13 @@
+<?php 
+header("Content-type:application/json");
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+    $id = $_GET['id'];
+    try {
+        require_once '../../config/connection.php';
+        include '../functions.php';
+        echo json_encode($getOneLink($id));
+    } catch (PDOException $th) {
+        echo json_encode($th->getMessage());
+        http_response_code(500);
+    }
+}
