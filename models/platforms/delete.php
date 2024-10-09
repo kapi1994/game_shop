b4f2618f2b70f6e9edc3b30e44541604e3f7ff3b
Nemanja Jovicic <nemanjajovicic13@gmail.com>
2024-10-09 19:50:44 +0200

    Soft delete for platforms entity

---
+++ models/platforms/delete.php
@@ -0,0 +1,15 @@
+<?php 
+header("Content-type:application/json");
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+    $id = $_POST['id'];
+    $status = $_POST['status'];
+    try {
+        require_once "../../config/connection.php";
+        include "../functions.php";
+        softDelete("platforms", $status, $id    );
+        echo json_encode($returnAfterDelete("platforms", $id));
+    } catch (PDOException $th) {
+        echo json_encode($th->getMessage());
+        http_response_code(500);
+    }
+}else http_response_code(404);
