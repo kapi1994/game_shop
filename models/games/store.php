91ae90bad3832b618a716f2318aaa21a8b428793
Nemanja Jovicic <nemanjajovicic13@gmail.com>
2024-10-24 18:13:21 +0200

    functionality to store new game

---
+++ models/games/store.php
@@ -0,0 +1,37 @@
+<?php 
+header("Content-type:application/json");
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $publisher = $_POST['publisher'];
+    $genres = explode(",", $_POST['genres']);
+    $trailer_url = $_POST['trailer'];
+
+
+    include '../validations.php';
+    include '../functions.php';
+
+    $gameFormValidation = gameFormValidation($name, $description, $publisher, $genres, $trailer_url);
+    if(count($gameFormValidation) > 0)
+        printErrors($gameFormValidation);
+    else{
+        try {
+            require_once '../../config/connection.php';
+            $gameNameCheck = $checkGameName($name);
+            if($gameNameCheck){
+                echo json_encode("Game with this name is allready taken");
+                http_response_code(409);
+            }else{
+                insertGame($name, $description, $publisher, $genres, $trailer_url);
+                echo json_encode([
+                    'data' => getAllGames(),
+                    'message' => "New game has been inserted"
+                ]);
+                http_response_code(201);
+            }
+        } catch (PDOException $th) {
+            echo json_encode($th->getMessage());
+            http_response_code(500);
+        }
+    }
+}else http_response_code(404);
